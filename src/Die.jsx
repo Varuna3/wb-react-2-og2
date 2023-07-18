e17aa967d1a47375c2fd78159537b86cf0b4cb26
Payton Kentch <frankrlystan@gmail.com>
2023-07-18 11:14:37 -0600

    finished {dice, movie} demos

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,8 +1,22 @@
-import { useState } from 'react';
-import './dice.css';
+import { useState } from 'react'
+import './dice.css'
 
 function getRandomNum(upperLimit) {
-  return Math.ceil(Math.random() * upperLimit);
+  return Math.ceil(Math.random() * upperLimit)
 }
 
+export default function Die(props) {
+  const [rolledNum, setRolledNum] = useState('?')
 
+  const roll = () => {
+    const result = getRandomNum(props.sides)
+    setRolledNum(result)
+  }
+
+  return (
+    <button type='button' className='die' onClick={roll}>
+      <i>sides={props.sides}</i>
+      <b>{rolledNum}</b>
+    </button>
+  )
+}
--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -1,11 +1,37 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 export default function MovieList() {
-  const [movies, setMovies] = useState(['Alien', 'Predator', 'Alien Vs. Predator']);
+  const [movies, setMovies] = useState([
+    'Alien',
+    'Predator',
+    'Alien Vs. Predator',
+  ])
   // This initializes our state value movies as an array with 3 items.
 
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState('')
   // This will be used to hold the value of our input box.
 
-  
+  const moviesDisplay = movies.map(movie => <li key={movie}>{movie}</li>)
+
+  return (
+    <>
+      <h2>Movie List</h2>
+      <ol>{moviesDisplay}</ol>
+      <input
+        style={{ marginLeft: '15px' }}
+        value={inputValue}
+        type='text'
+        onChange={e => setInputValue(e.target.value)}
+      />
+      <button
+        style={{ marginLeft: '15px' }}
+        onClick={() => {
+          setMovies([...movies, inputValue])
+          setInputValue('')
+        }}
+      >
+        Add Movie
+      </button>
+    </>
+  )
 }
